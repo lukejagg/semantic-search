0a2832cc54184e6949865fde46c65b9d4c372063
Zach Tang <zachtangoj@gmail.com>
2023-07-28 15:40:52 -0700

    handle different actions

--- backend/api/action/views.py
+++ backend/api/action/views.py
@@ -3,7 +3,32 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from action.models import Action
 from action.serializers import ActionSerializer
+from enum import Enum
 
+class ActionType(Enum):
+    SEARCH = 'search'
+    AUTOCOMPLETE = 'autocomplete'
+    CLICK_LINK = 'click_link'
+
+def handle_search(search):
+    # Input search into Luke's ML model to get semantic responses
+    text_response = ''
+    # text_response = ml_model.input(search)
+
+    return Response(text_response, status=200)
+
+def handle_autocomplete(search):
+    # Input search into Luke's ML model to get list of strings for autocomplete
+    autocomplete_strings = ['']
+    # autocomplete_strings = ml_model.input(search)
+
+    return Response(autocomplete_strings, status=200)
+
+def handle_click_link(link):
+    # Input link into Luke's ML model for improvement
+    # ml_model.input(link)
+
+    return Response(status=200)
 
 # CRUD logic
 @api_view(['POST'])
@@ -12,9 +37,17 @@ def post_action(request):
         serializer = ActionSerializer(data = request.data)
 
         if serializer.is_valid():
-            print(serializer.data)
-            # serializer.save()
+            action = serializer.data
+            
+            if action['type'] == ActionType.SEARCH:
+                return handle_search(action['body'])
+            
+            if action['type'] == ActionType.AUTOCOMPLETE:
+                return handle_autocomplete(action['body'])
+            
+            if action['type'] == ActionType.CLICK_LINK:
+                return handle_click_link(action['body'])            
 
-            return Response(serializer.data, status=201)
+            return Response(serializer.data, status=200)
         
         return Response(serializer.errors, status=400)
