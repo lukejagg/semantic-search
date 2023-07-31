735479742b7fdaf550e3102bf8090c59ce3d144a
--local <--local>
2023-07-30 19:09:21 -0700

    ayutocomplet

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -3,8 +3,11 @@ import {
   TextInputProps,
   ActionIcon,
   useMantineTheme,
+  Loader,
+  Autocomplete,
 } from "@mantine/core";
 import { IconSearch, IconArrowRight, IconArrowLeft } from "@tabler/icons-react";
+import { UseQueryResult, useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
@@ -12,11 +15,41 @@ interface SearchBarProps extends TextInputProps {
   setFocus?: (focus: boolean) => void;
 }
 
+type Autocomplete = string[];
+
+export const useAutocomplete = (
+  search: string
+): UseQueryResult<Autocomplete> => {
+  return useQuery(
+    ["autocomplete", search],
+    async () => {
+      const response = await fetch(
+        `http://127.0.0.1:8000/action/autocomplete/${search}`
+      );
+      const data = await response.json();
+      return data as Autocomplete;
+    },
+    { keepPreviousData: true, refetchOnWindowFocus: false }
+  );
+};
+
 export default function SearchBar(props: SearchBarProps) {
   const theme = useMantineTheme();
   const [query, setQuery] = useState("");
   const navigate = useNavigate();
   let { query: pre } = useParams();
+  const [delay, setDelay] = useState(false);
+
+  const { data, isFetching } = useAutocomplete(query || "-");
+
+  useEffect(() => {
+    if (isFetching && query) {
+      setDelay(true);
+      setTimeout(() => {
+        setDelay(false);
+      }, 250);
+    }
+  }, [query]);
 
   useEffect(() => {
     if (pre) {
@@ -33,11 +66,18 @@ export default function SearchBar(props: SearchBarProps) {
   };
 
   return (
-    <TextInput
-      icon={<IconSearch size="1.1rem" stroke={1.5} />}
+    <Autocomplete
+      icon={
+        query && (isFetching || delay) ? (
+          <Loader size="1rem" />
+        ) : (
+          <IconSearch size="1.1rem" stroke={1.5} />
+        )
+      }
       radius="xl"
       size="md"
       value={query}
+      data={query && data ? data : []}
       width="100%"
       onFocus={() => {
         if (props.setFocus) {
@@ -67,13 +107,13 @@ export default function SearchBar(props: SearchBarProps) {
         </ActionIcon>
       }
       rightSectionWidth={42}
-      onChange={(event) => setQuery(event.currentTarget.value)}
+      onChange={(event) => setQuery(event)}
+      filter={() => true}
       onKeyDown={(event) => {
         if (event.key === "Enter") {
           search();
         }
       }}
-      {...props}
     />
   );
 }
